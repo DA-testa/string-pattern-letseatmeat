53386527e56f630cfe708410fd66ef498682edee
Kirils Aleksejevs 221RDB139 <102963681+letseatmeat@users.noreply.github.com>
2023-04-16 22:42:32 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -2,40 +2,41 @@
 
 def read_input():
     try:
+        # Read input from keyboard or file
         input_type = input().rstrip().upper()
         if input_type == 'I':
-            pattern = input().rstrip() 
-            text = input().rstrip() 
-        elif input_type == 'F': 
-            filename = input().rstrip()
-            with open(f"tests/{filename}", 'r') as file: 
+            pattern = input().rstrip()
+            text = input().rstrip()
+        elif input_type == 'F':
+            with open('tests/06', 'r') as file:
                 pattern = file.readline().rstrip()
                 text = file.readline().rstrip()
         else:
-            raise ValueError("Wrong input, must be I or F.")
+            raise ValueError('Wrong input, must be I or F')
     except ValueError:
-        print("Wrong input, must be I or F.")
-        pattern, text = '', ''
+        print('Wrong input, must be I or F')
+        raise
+    
     return pattern, text
-
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-
 def get_occurrences(pattern, text):
-    pattern_length = len(pattern)
-    pattern_hash = hash(pattern) if pattern else 0
-    text_length = len(text)
-    text_hash = hash(text[:pattern_length]) if text else 0
-    occurrences = []
-    for i in range(text_length - pattern_length + 1):
-        if pattern_hash == text_hash and text[i:i+pattern_length] == pattern:
-            occurrences.append(i)
-        if i + pattern_length < text_length:
-            text_hash = hash(text[i+1:i+pattern_length+1])
-    return occurrences
+    # Rabin-Karp algorithm for pattern matching
+    p_len = len(pattern)
+    pattern_hash = hash(pattern)
+    text_len = len(text)
+    text_hash = hash(text[:p_len])
+    output = []
+    
+    for i in range(text_len - p_len + 1):
+        if text_hash == pattern_hash and text[i:i+p_len] == pattern:
+            output.append(i)
+        if i + p_len < text_len:
+            text_hash = hash(text[i+1:i+p_len+1])
+    
+    return output
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
